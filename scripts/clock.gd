f3daf1193e873ae3f49b7bcdec0f121356db7f20
Tim (Tim) <Benaiah.dotson@outlook.com>
2025-10-10 21:23:10 -0500

    added time
    yeah also if you're in the military I gotchu

--- scripts/clock.gd
+++ scripts/clock.gd
@@ -2,6 +2,12 @@ extends Control
 
 @onready var label: Label = $ClockTexture/Label
 
+var date = Time.get_datetime_dict_from_system()
+var time = Time.get_time_dict_from_system()
+
+var military_time = false
+var relation_to_meridian = "AM"
+
 var units = [
 	{"Time Since 1970" = [
 		"Seconds",
@@ -28,9 +34,29 @@ var units = [
 ]
 
 
+
 func _ready() -> void:
 	pass
 
 
 func _physics_process(_delta: float) -> void:
-	label.text = str(floori(Time.get_unix_time_from_system()))
+	date = Time.get_datetime_dict_from_system()
+	time = Time.get_time_dict_from_system()
+	
+	var hour = time.hour
+	var minute = time.minute
+	
+	if !military_time and time.hour > 12:
+		hour = time.hour - 12
+		relation_to_meridian = "PM"
+	
+	if time.minute < 10:
+		minute = str("0", time.minute)
+		
+	
+	if military_time:
+		label.text = str(hour, ":", minute, ":", time.second)
+	else:
+		label.text = str(hour, ":", minute, ":", time.second, " ", relation_to_meridian)
+	
+	
